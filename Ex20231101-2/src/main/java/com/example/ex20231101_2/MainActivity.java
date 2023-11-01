592a8710f0417fbdc7fdeeb1a706b9732860159c
Qinsp <qinsprite@outlook.com>
2023-11-01 11:39:34 +0800

    Initial commit

--- Ex20231101-2/src/main/java/com/example/ex20231101_2/MainActivity.java
+++ Ex20231101-2/src/main/java/com/example/ex20231101_2/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.ex20231101_2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView output;
+    private RadioGroup radioGroup;
+    private RadioButton rdb01, rdb02, rdb03, rdb04;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        output = findViewById(R.id.lbloutput);
+        radioGroup = findViewById(R.id.rdgsteak);
+        rdb01 = findViewById(R.id.rdgRare);
+        rdb02 = findViewById(R.id.rdgmedium);
+        rdb03 = findViewById(R.id.rdgmedwell);
+        rdb04 = findViewById(R.id.rdgsmwelldone);
+        radioGroup.setOnCheckedChangeListener(listener);
     }
+
+    private RadioGroup.OnCheckedChangeListener listener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, int i) {
+            if (i == rdb01.getId()) {
+                output.setText(rdb01.getText());
+            } else if (i == rdb02.getId()) {
+                output.setText(rdb02.getText());
+            } else if (i == rdb03.getId()) {
+                output.setText(rdb03.getText());
+            } else {
+                output.setText(rdb04.getText());
+            }
+        }
+    };
 }
---
+++ Ex20231101-3/src/main/java/com/example/ex20231101_3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ex20231101_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
